c82120baad618ecc1678ef3e182294894c8f5432
Dylanmeyer <61458670+Dylanmeyer@users.noreply.github.com>
2020-11-16 23:47:01 +0200

    Add files via upload

--- Rainbow ads/main.js
+++ Rainbow ads/main.js
@@ -1,3 +1,12 @@
+/*window.addEventListener("scroll", function(){
+    let menuArea = document.getElementById('menu-area');
+    if (window.pageYOffset > 0){
+        menuArea.classList.add("cus-nav");
+    }else{
+        menuArea.classList.remove("cus-nav")
+    }
+})*/
+
 $(function () {
     $(window).on('scroll', function () {
         if ( $(window).scrollTop() > 10 ) {
